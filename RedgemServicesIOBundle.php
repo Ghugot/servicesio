3df90ab65b6ed466ed3cd5d57c70be7cfe353996
Guillaume HUGOT <guillaume.hugot@gmail.com>
2015-10-02 20:24:15 +0200

    Bundle declaration

---
+++ RedgemServicesIOBundle.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Bundle declaration
+ *
+ * PHP Version 5
+ *
+ * @category Bundle
+ * @package  Redgem\ServicesIOBundle
+ * @author   Guillaume HUGOT <guillaume.hugot@gmail.com>
+ * @license  MIT
+ * @link     http://github.com/ghugot/ServicesIO
+ */
+
+namespace Redgem\ServicesIOBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle declaration
+ *
+ * @category Bundle
+ * @package  Redgem\ServicesIOBundle
+ * @author   Guillaume HUGOT <guillaume.hugot@gmail.com>
+ * @license  MIT
+ * @link     http://github.com/ghugot/ServicesIO
+ */
+class RedgemServicesIOBundle extends Bundle
+{
+
+}
